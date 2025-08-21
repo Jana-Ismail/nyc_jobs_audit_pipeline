72cb4e410fb877f69cbb202054859f3c252d52a8
Jana Ismail <jana.ismail.nss@gmail.com>
2025-08-21 16:14:48 +0000

    stg_nyc_job_postings.sql for dbt cleaning

--- dbt/dbt_duckdb_project/models/staging/stg_nyc_job_postings.sql
+++ dbt/dbt_duckdb_project/models/staging/stg_nyc_job_postings.sql
@@ -1,18 +1,51 @@
-config {{
-  materialized = "incremental",
-  tags=['staging', 'silver', 'nyc_job_postings']
-}
+{{
+    config(
+        materialized='view',
+        schema='silver'
+    )
+}}
 
 SELECT
-  job_id,
-  agency,
-  business_title,
-  career_level,
-  salary_range_from,
-  salary_range_to,
-  salary_frequency,
-  DATE(posting_date) AS posting_date
+    job_id,
+    agency,
+    business_title,
+    career_level,
 
-FROM {{ source('bronze', 'nyc_job_postings') }}
+    CASE
+        WHEN salary_range_from is not null and trim(salary_range_from) != ''
+        THEN CAST(REGEXP_REPLACE(salary_range_from, '[$,]', '', 'g') AS DECIMAL(12,2))
+        ELSE NULL
+    END AS salary_range_from,
+
+    CASE
+        WHEN salary_range_to is not null and trim(salary_range_to) != ''
+        THEN CAST(REGEXP_REPLACE(salary_range_to, '[$,]', '', 'g') AS DECIMAL(12,2))
+        ELSE NULL
+    END AS salary_range_to,
+
+    salary_frequency,
+    CAST(posting_date AS DATE) AS posting_date,
+    CAST(post_until AS DATE) AS post_until,
+    CAST(process_date)
+
+    CASE
+        WHEN post_until is not null and posting_date is not null
+        THEN CAST(post_until AS DATE) - CAST(posting_date AS DATE)
+        ELSE {{ var('default_posting_duration_days')}}
+    END AS posting_duration,
+
+    trim(lower(business_title)) AS business_title_clean,
+    trim(upper(agency)) AS agency_clean,
+    posting_date
+
+FROM {{ source('bronze', 'raw_nyc_job_postings')}}
 WHERE
-  EXTRACT(YEAR FROM DATE(posting_date)) IN (2024, 2025)
+    (
+        EXTRACT(YEAR FROM CAST(POSTING_DATE AS DATE)) in ({{ var('target_years') | join(', ')}})
+    or
+    (posting_date is null and EXTRACT(YEAR FROM CAST(process_date AS DATE)) in ({{ var('target_years') | join(', ')}}))
+    )
+    
+AND job_id IS NOT NULL
+AND business_title IS NOT NULL
+AND agency IS NOT NULL
