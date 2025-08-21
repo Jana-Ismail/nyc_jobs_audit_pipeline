5493cd31709bf2dece388e295509d532b6774cea
Jana Ismail <jana.ismail.nss@gmail.com>
2025-08-21 18:35:33 +0000

    dbt silver staging script for nyc_job_postings

--- dbt/dbt_duckdb_project/models/staging/stg_nyc_job_postings.sql
+++ dbt/dbt_duckdb_project/models/staging/stg_nyc_job_postings.sql
@@ -10,23 +10,12 @@ SELECT
     agency,
     business_title,
     career_level,
-
-    CASE
-        WHEN salary_range_from is not null and trim(salary_range_from) != ''
-        THEN CAST(REGEXP_REPLACE(salary_range_from, '[$,]', '', 'g') AS DECIMAL(12,2))
-        ELSE NULL
-    END AS salary_range_from,
-
-    CASE
-        WHEN salary_range_to is not null and trim(salary_range_to) != ''
-        THEN CAST(REGEXP_REPLACE(salary_range_to, '[$,]', '', 'g') AS DECIMAL(12,2))
-        ELSE NULL
-    END AS salary_range_to,
-
+    salary_range_from,
+    salary_range_to,
     salary_frequency,
     CAST(posting_date AS DATE) AS posting_date,
     CAST(post_until AS DATE) AS post_until,
-    CAST(process_date)
+    CAST(process_date AS DATE) AS process_date,
 
     CASE
         WHEN post_until is not null and posting_date is not null
